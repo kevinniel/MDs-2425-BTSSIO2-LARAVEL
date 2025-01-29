8c598c87cdc6df6c11f4361cb819feebff3cd021
kevinniel <k.niel.pro@gmail.com>
2025-01-29 12:19:36 +0100

    update

--- clickneat/app/Models/Restaurant.php
+++ clickneat/app/Models/Restaurant.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    //
+
+    use HasFactory;
+
+    protected $table = "restaurants";
+
+    protected $fillable = [
+        "name"
+    ];
 }
